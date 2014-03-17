84aa520c68f61db837861fb7f91fcbd4ed96707a
Glen Maddern <glenmaddern@gmail.com>
2014-03-16 19:04:17 -0600

    init bespoke after polymer and problems go away

--- gulpfile.js
+++ gulpfile.js
@@ -9,64 +9,59 @@ var $ = require('gulp-load-plugins')();
 
 // Styles
 gulp.task('styles', function () {
-    return gulp.src([
-        'src/bower_components/prism/themes/prism-twilight.css',
-        'src/styles/**/*.scss'])
-        .pipe($.sass({
-          style: 'expanded',
-          includePath: ['src/bower_components']
-        }))
-        .pipe($.concat('styles.css'))
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('public/styles'))
-        .pipe($.size());
+  return gulp.src([
+      'src/bower_components/prism/themes/prism-twilight.css',
+      'src/styles/**/*.scss'])
+    .pipe($.sass({
+      style: 'expanded',
+      includePath: ['src/bower_components']
+    }))
+    .pipe($.concat('styles.css'))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest('public/styles'))
+    .pipe($.size());
 });
 
 // Scripts
 gulp.task('scripts', function () {
-    return gulp.src([
-            'src/bower_components/bespoke.js/dist/bespoke.min.js',
-            'src/bower_components/bespoke-keys/dist/bespoke-keys.min.js',
-            'src/bower_components/bespoke-touch/dist/bespoke-touch.min.js',
-            'src/bower_components/bespoke-scale/dist/bespoke-scale.min.js',
-            'src/bower_components/bespoke-hash/dist/bespoke-hash.min.js',
-            'src/bower_components/bespoke-state/dist/bespoke-state.min.js',
-            'src/bower_components/bespoke-bullets/dist/bespoke-bullets.min.js',
-            'src/bower_components/prism/prism.js',
-            'src/bower_components/prism/components/prism-bash.js',
-            'src/bower_components/prism/components/prism-css.js',
-            'src/bower_components/prism/components/prism-css-extras.js',
-            'src/scripts/**/*.js'])
+  return gulp.src([
+      'src/bower_components/bespoke.js/dist/bespoke.min.js',
+      'src/bower_components/bespoke-keys/dist/bespoke-keys.min.js',
+      'src/bower_components/bespoke-touch/dist/bespoke-touch.min.js',
+      'src/bower_components/bespoke-scale/dist/bespoke-scale.min.js',
+      'src/bower_components/bespoke-hash/dist/bespoke-hash.min.js',
+      'src/bower_components/bespoke-state/dist/bespoke-state.min.js',
+      'src/bower_components/bespoke-bullets/dist/bespoke-bullets.min.js',
+      'src/bower_components/prism/prism.js',
+      'src/bower_components/prism/components/prism-bash.js',
+      'src/bower_components/prism/components/prism-css.js',
+      'src/bower_components/prism/components/prism-css-extras.js',
+      'src/scripts/**/*.js'])
 //        .pipe($.jshint('.jshintrc'))
 //        .pipe($.jshint.reporter('default'))
-        .pipe($.concat('scripts.js'))
-        .pipe(gulp.dest('public/scripts'))
-        .pipe($.size());
+    .pipe($.concat('scripts.js'))
+    .pipe(gulp.dest('public/scripts'))
+    .pipe($.size());
 });
 
 // HTML
 gulp.task('html', function () {
-    return gulp.src('src/*.jade')
-      .pipe($.jade())
-      .pipe(gulp.dest('public'))
-      .pipe($.size());
+  return gulp.src('src/*.jade')
+    .pipe($.jade())
+    .pipe(gulp.dest('public'))
+    .pipe($.size());
 });
 
 // Images
 gulp.task('images', function () {
-    return gulp.src('src/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('public/images'))
-        .pipe($.size());
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('public/images'))
+    .pipe($.size());
 });
 
 // Clean
 gulp.task('clean', function () {
-    return gulp.src(['public/*'], {read: false}).pipe($.clean());
+  return gulp.src(['public/*'], {read: false}).pipe($.clean());
 });
 
 gulp.task('x-gif', function () {
@@ -86,44 +81,44 @@ gulp.task('build', ['html', 'styles', 'scripts', 'images', 'x-gif', 'copy']);
 
 // Default task
 gulp.task('default', ['clean'], function () {
-    gulp.start('build');
+  gulp.start('build');
 });
 
 // Connect
 gulp.task('connect', $.connect.server({
-    root: ['public'],
-    port: 8000,
-    livereload: true
+  root: ['public'],
+  port: 8000,
+  livereload: true
 }));
 
 // Watch
 gulp.task('watch', ['build', 'connect'], function () {
-    // Watch for changes in `src` folder
-    gulp.watch([
-        'public/**/*'
-    ], function(event) {
-        return gulp.src(event.path)
-            .pipe($.connect.reload());
-    });
+  // Watch for changes in `src` folder
+  gulp.watch([
+    'public/**/*'
+  ], function (event) {
+    return gulp.src(event.path)
+      .pipe($.connect.reload());
+  });
 
-    // Watch .jade files
-    gulp.watch('src/*.jade', ['html']);
+  // Watch .jade files
+  gulp.watch('src/*.jade', ['html']);
 
-    // Watch .scss files
-    gulp.watch('src/styles/**/*.scss', ['styles']);
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
 
-    // Watch .js files
-    gulp.watch('src/scripts/**/*.js', ['scripts']);
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
 
-    // Watch image files
-    gulp.watch('src/images/**/*', ['images']);
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
 
-    // Watch x-gif
-    gulp.watch('src/bower_components/x-gif/dist/**/*', ['x-gif']);
+  // Watch x-gif
+  gulp.watch('src/bower_components/x-gif/dist/**/*', ['x-gif']);
 
-    // Watch fonts
-    gulp.watch('src/fonts/**', ['copy']);
+  // Watch fonts
+  gulp.watch('src/fonts/**', ['copy']);
 
-    // Watch bower files
-    gulp.watch('src/bower_components/*', ['build']);
+  // Watch bower files
+  gulp.watch('src/bower_components/*', ['build']);
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,28 +19,30 @@ bespoke.plugins.delaySrc = function (deck, options) {
 }
 
 bespoke.plugins.startXGif = function (deck, options) {
-  var gifs = deck.slides.map(function (slide) {
-      return [].slice.call(slide.querySelectorAll('x-gif[stopped]'), 0);
-    }),
-    setStopped = function (stopped) {
-      return function (slide) {
-        gifs[slide.index].map(function (object) {
-          object.stopped = stopped;
-        })
-      }
-    };
+  var setStopped = function (stopped) {
+    return function (slide) {
+
+      window.wat = slide.slide.querySelectorAll('x-gif')
+      return [].map.call(slide.slide.querySelectorAll('x-gif'), function (gif) {
+        console.log(gif, stopped)
+        gif.stopped = stopped;
+      });
+    }
+  };
 
   deck.on('activate', setStopped(null));
   deck.on('deactivate', setStopped(true));
 }
 
-bespoke.from('article', {
-  keys: true,
-  touch: true,
-  scale: true,
-  hash: true,
-  state: true,
-  bullets: true,
-  delaySrc: true,
-  startXGif: true
-});
+window.addEventListener('polymer-ready', function () {
+  bespoke.from('article', {
+    keys: true,
+    touch: true,
+    scale: true,
+    hash: true,
+    state: true,
+    bullets: true,
+    delaySrc: true,
+    startXGif: true
+  });
+})
